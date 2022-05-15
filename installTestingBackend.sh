3c5033847d68257903f13dde197eb525a3d4f280
Iqwertz <36999445+Iqwertz@users.noreply.github.com>
2022-05-15 23:45:19 +0200

    updated installing backend

--- installTestingBackend.sh
+++ installTestingBackend.sh
@@ -5,4 +5,11 @@ sudo mkdir testingBackend
 cd testingBackend
 sudo wget -O - https://github.com/Iqwertz/Depictor/archive/$branch.tar.gz | sudo tar -xz --strip=2 "Depictor-$branch/Backend"
 npm i
-npm run start
+sudo sed -i -e 's/3001/3333/g' ./src/enviroment.ts
+sudo testingBackend/scripts/setUSBChmod.sh
+sudo chmod +x chmodScripts.sh
+sudo ./chmodScripts.sh
+sudo find ./ -type d -exec chmod 755 {} \;
+sudo chmod -R a+rw ./*
+sudo chmod a+rw /dev/ttyACM0
+sudo nodemon
