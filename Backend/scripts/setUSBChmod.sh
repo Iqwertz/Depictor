906fd8c08fd2772036976c777299c5c381c46822
Iqwertz <36999445+Iqwertz@users.noreply.github.com>
2022-07-24 23:45:19 +0200

    improved gcode preview generation during upload

--- Backend/scripts/setUSBChmod.sh
+++ Backend/scripts/setUSBChmod.sh
@@ -2,9 +2,9 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 source $parent_path/portConfig.sh
 #check if usb port is connected
-if [ -z "$serialPort" ]; then
-    echo "No USB port connected"
-    else
-    sudo chmod a+rw $serialPort
+sudo chmod a+rw $serialPort
+if [ $? = 0 ]; then
     echo "USB port $serialPort is now accessible"
+else
+    echo "No USB port connected"
 fi
