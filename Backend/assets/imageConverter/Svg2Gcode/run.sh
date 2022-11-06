c44eab4b34ff3bd16c2c2035822cd57031357587
iqwertz <juliushussl@gmail.com>
2022-11-06 11:49:46 +0100

    please work

--- Backend/assets/imageConverter/Svg2Gcode/run.sh
+++ Backend/assets/imageConverter/Svg2Gcode/run.sh
@@ -4,6 +4,6 @@ cd "$parent_path"
 
 chmod +x ./bin/svg2gcode
 mkdir -p output
-./bin/svg2gcode --output output/gcode.nc --image output/ 
+./bin/svg2gcode --output output/gcode.nc
 cp input/image.svg output/image.svg
 mv output/image.png output/preview.png
