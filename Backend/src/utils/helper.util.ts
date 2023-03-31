3027ab7a1cfcd9e839ce0b4d81786d8d4bfa4d9f
iqwertz <juliushussl@gmail.com>
2023-03-31 11:51:12 +0200

    finished commenting backend

--- Backend/src/utils/helper.util.ts
+++ Backend/src/utils/helper.util.ts
@@ -1,10 +1,21 @@
-//imports
+////////////////////helper.util/////////////////////////
+// this file contains helper functions that are used in multiple places
+// exports:
+//  loadConfig: (loads the converter config file and returns it)
+//  chmodConverters: (sets the execute permission for all converter run.sh files)
+////////////////////////////////////////////////////////
 
+//imports
 import { logger } from "./logger.util";
 import { execFile } from "child_process";
 import { Config } from "../middleware/converter.middleware";
 const fs = require("fs");
 
+/**
+ * loadConfig()
+ * loads the assets/config.json file and returns it if it exists
+ * @returns {Config | undefined} - the config file or undefined if it doesnt exist
+ */
 export function loadConfig(): Config | undefined {
   if (fs.existsSync("assets/config.json")) {
     logger.info("found config");
@@ -16,6 +27,10 @@ export function loadConfig(): Config | undefined {
   }
 }
 
+/**
+ * chmodConverters
+ * set execute permission for all converter run.sh files to make them executable
+ */
 export function chmodConverters() {
   logger.info("chmoding converters");
   let config: Config | undefined = loadConfig();
